367a5d916664c505b495deb82c8fdfd00d33ffdb
Sallo0 <simiklassnik@yandex.ru>
2021-04-24 04:02:27 +0500

    TEXT

---
+++ MMGame/Trouble.cs
@@ -0,0 +1,27 @@
+﻿using System.Drawing;
+
+namespace MMGame
+{
+    public class Trouble
+    {
+        public int x;
+        public int y;
+        public Bitmap TroubleImage;
+        public Rectangle HitBox;
+
+        public Trouble(int x, int y, Bitmap troubleImage)
+        {
+            this.x = x;
+            this.y = y;
+            TroubleImage = troubleImage;
+            HitBox = new Rectangle(x, y, TroubleImage.Width, TroubleImage.Height);
+        }
+        
+
+        public void Incident(Player player)
+        {
+            if (HitBox.IntersectsWith(player.HitBox))
+                player.IsAlive = false;
+        }
+    }
+}
