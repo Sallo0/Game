a445906859676787b24cc445687171fe536ef898
Sallo0 <simiklassnik@yandex.ru>
2021-05-13 02:40:08 +0500

    add tests

---
+++ MMGame/Tests.cs
@@ -0,0 +1,26 @@
+﻿using System.Drawing;
+using NUnit.Framework;
+
+
+namespace MMGame
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void IsSizeInitializationCorrect()
+        {
+            var gaming = new Form1();
+            Assert.AreEqual(1500, gaming.Width);
+            Assert.AreEqual(630, gaming.Height);
+        }
+
+        [Test]
+        public void IsHitboxesCorrect()
+        {
+            var player = new Player(0,0);
+            var trouble = new Trouble(0, 0, new Bitmap(Tools.GetFullPath("TroubleChair.png")));
+            Assert.AreEqual(true, player.HitBox.IntersectsWith(trouble.HitBox));
+        }
+    }
+}
